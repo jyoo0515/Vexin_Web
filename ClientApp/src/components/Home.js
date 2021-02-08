2fe4f6a21349f2eefabbc4c945e5f97f36eb4489
jyoo <jyoo@yonsei.ac.kr>
2021-02-08 13:54:15 +0900

    Initial

---
+++ ClientApp/src/components/Home.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap/lib';
+import './Home.css';
+import thumb0 from './img/Eng_Test.png';
+import thumb1 from './img/21_Guide.png';
+import thumb2 from './img/Scholarship.png';
+import thumb3 from './img/bg.jpg';
+import thumb4 from './img/20_Guide.png';
+import thumb5 from './img/Vector_Youtube.png';
+import tempImg from './img/Coming_Soon.png';
+
+export class Home extends Component {
+  static displayName = Home.name;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+      title: "Title",
+      description: "Description",
+      url: "url",
+      disabled: false
+    };
+  }
+
+  toggle = () => {
+    this.setState({isOpen: !this.state.isOpen});
+  }
+
+  selectInfo = (i) => {
+    const linkInfo = [
+      {title: "공대능력평가", description: "많은 새내기들을 혼란에 빠뜨렸던 공대능력평가, 드디어 공개됩니다.\n즐겨주세요 :)",
+        url: "https://engineerexam.azurewebsites.net/", disabled: false},
+      {title: "새내기자료집", description: "대학생활이 낯선 신입생 여러분들을 준비한 공과대학 입문 가이드!\n\n준비 중입니다!",
+        url: "https://engineerexam.azurewebsites.net/", disabled: true},
+      {title: "Vector 장학금 플랫폼 beta", description: "찾아보기 힘들었던 장학금, 이제는 하나의 플랫폼에서 손쉽게\n확인하세요 :)",
+        url: "https://www.notion.so/Vector-beta-765eef0e0e4f4238a8287fd50fb899be", disabled: false},
+      {title: "Battle Campus", description: "송도에서 펼쳐지는 좀비게임\n\nComing Soon...", url: "http://13.125.206.169/play", disabled: true},
+      {title: "20학번 안내벡서", description: "신촌 새내기가 된 20학번들을 위한 유익한 안내Vec서\n\n준비 중입니다!",
+        url: "https://engineerexam.azurewebsites.net/", disabled: true},
+      {title: "Vector Youtube", description: "공과대학 OT, 즐거운 컨텐츠, 유익한 정보가 영상으로 제공됩니다.",
+        url: "https://www.youtube.com/channel/UCwe1AuUxppCfrORlrWjdYjQ", disabled: false}
+    ]
+    this.setState({
+      title: linkInfo[i].title,
+      description: linkInfo[i].description,
+      url: linkInfo[i].url,
+      disabled: linkInfo[i].disabled
+    });
+  }
+
+  render () {
+    return (
+      <div>
+        <h3 className="section-title">프로그램 목록</h3>
+        <div className="card-list">
+          <Card className={0} onClick={() => { this.selectInfo(0); this.toggle(); }} />
+          <Card className={1} onClick={() => { this.selectInfo(1); this.toggle(); }} />
+          <Card className={2} onClick={() => { this.selectInfo(2); this.toggle(); }} />
+        </div>
+
+        <div className="card-list">
+          <Card className={6} onClick={() => { this.selectInfo(3); this.toggle(); }} />
+          <Card className={4} onClick={() => { this.selectInfo(4); this.toggle(); }} />
+          <Card className={5} onClick={() => { this.selectInfo(5); this.toggle(); }} />
+        </div>
+        
+        <Modal isOpen={this.state.isOpen} toggle={this.toggle}>
+          <ModalHeader toggle={this.toggle}>{this.state.title}</ModalHeader>
+          <ModalBody>
+            <br/>
+            {this.state.description.split('\n').map( line => {
+              return (<span>{line}<br/></span>)
+            })}
+            <br/>
+          </ModalBody>
+          <ModalFooter>
+            <a href={this.state.url}>
+              <Button color="warning" disabled={this.state.disabled}>이동하기</Button>
+            </a>
+            <Button color="secondary" onClick={this.toggle}>닫기</Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export class Card extends Component {
+  render() {
+    const imgList = [thumb0, thumb1, thumb2, thumb3, thumb4, thumb5, tempImg];
+
+    return(
+      <div className="card-container" onClick={this.props.onClick}>
+        <div className="card">
+          <div className="img-gradient"></div>
+          <img src={imgList[this.props.className]} alt="Background image"></img>
+          <div className="play-button"></div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ ClientApp/src/components/NavMenu.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import './NavMenu.css';
+
+import logo from './img/logo.png';
+
+export class NavMenu extends Component {
+  static displayName = NavMenu.name;
+
+  constructor (props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleNavbar () {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+
+  render () {
+    return (
+      <header>
+        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
+          <Container>
+            <NavbarBrand tag={Link} to="/">
+              <img className="logo" src={logo} alt="Nexin Logo"/>
+            </NavbarBrand>
+            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
+              <ul className="navbar-nav flex-grow">
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/">
+                    연세대학교 제58대 공과대학 학생회 Vector
+                  </NavLink>
+                </NavItem>
+                {/* <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/about">About</NavLink>
+                </NavItem>*/}
+              </ul>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </header>
+    );
+  }
+}
